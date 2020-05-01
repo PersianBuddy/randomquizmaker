7b41d0cd604272a4f1060aeffe083ee7a70356da
PersianBuddy <mnajafi0014@gmail.com>
2020-05-01 17:53:34 +0430

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# TODO: get a dictionary of state:capital
+
+# TODO: Create 30 different random quiz
+# do these for 30 times
+# TODO: Randomiz qustions:answers
+# TODO: save answer for fist question in a dictionary
+# TODO: create a list of answers and get 3 wrong answer form it
+# TODO: add qustion and answers to a string
+# TODO: save string of qustion and answers and answers into two files
